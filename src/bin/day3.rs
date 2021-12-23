6b9511ffc349ca20671c8c6e8143a6f1dc2acdf6
jackred <jackred@tuta.io>
2021-12-23 15:59:43 +0000

    UPDATE day3 part 2
    finally coding again
    brain is not dead also

--- src/bin/day3.rs
+++ src/bin/day3.rs
@@ -8,7 +8,7 @@ fn main() {
         .expect("Something went wrong reading the file");
     let lines: Vec<i32> = contents.lines().map(|s| i32::from_str_radix(s, 2).unwrap()).collect();
     println!("part 1: {}", part_1(&lines));
-    //println!("part 2: {}", part_2(&lines));    
+    println!("part 2: {}", part_2(&lines));    
 }
 
 
@@ -30,64 +30,34 @@ fn part_1(input:  &Vec<i32>) -> i32 {
     a * b
 }
 
-
-
-// // yes it is extremly ugly
-// // trying to figure out how to rust, but after a full day of work, I can barely think and solve the problem
-// // so doing a nnice thing in rust will come later
-// // my brain is dead
-// fn part_2(input: &Vec<i32>) -> i32 {
-//     let mut n = input.len() as i32;
-//     let mut m = input.len() as i32;
-//     let mut o_r: Vec<i32> = vec![];
-//     let mut c_r: Vec<i32> = vec![];
-//     let mut co2: Vec<i32> = vec![];
-//     let mut o2 : Vec<i32> = vec![];
-//     while n > 1 || m > 1 {
-// 	let res = input.into_iter().fold(vec![0, 0, -1,-1,0,0, 0], |mut acc, e| {
-// 	    if e[..o_r.len()] == o_r {
-// 		acc[0] += e[o_r.len()];
-// 		acc[2] = acc[4];
-// 		acc[5] += 1;
-// 	    }
-// 	    if e[..c_r.len()] == c_r {
-// 		acc[1] += e[c_r.len()];
-// 		acc[3] = acc[4];
-// 		acc[6] += 1;
-// 	    }
-// 	    acc[4] += 1;
-// 	    acc
-// 	});
-// 	o_r.push(if res[0] >= 0 {1} else {-1});
-// 	c_r.push(if res[1] >= 0 {-1} else {1});
-// 	if n != 1 { 
-// 	    if o_r.len() == input[0].len() {
-// 		n = 1;
-// 		o2 = o_r.clone();
-// 	    } else {
-// 		n = res[5];
-// 		o2 = input[res[2] as usize].clone();
-// 	    }
-// 	}
-// 	if m != 1 {
-// 	    if c_r.len() == input[0].len() {
-// 		m = 1;
-// 		co2 = c_r.clone();
-// 	    } else {
-// 		m = res[6];
-// 		co2 = input[res[3] as usize].clone();
-// 	    }
-// 	}	    
-//     };
-//     let o2 = o2.iter().fold(String::new(),|mut acc, &e| {
-// 	acc.push(if e > 0 {'1'} else {'0'});
-// 	acc
-//     });
-//     let co2 = co2.iter().fold(String::new(),|mut acc, &e| {
-// 	acc.push(if e > 0 {'1'} else {'0'});
-// 	acc
-//     });
-//     let o2 = i32::from_str_radix(o2.as_str(), 2).unwrap();
-//     let co2 = i32::from_str_radix(co2.as_str(), 2).unwrap();
-//     o2 * co2
-// }
+// better than before tbh
+fn part_2(input:  &Vec<i32>) -> i32 {
+    let a = (1..WIDTH+1).rev().fold((0,0), |mut acco2, i| {
+	let res = input.iter().fold((vec![0,0], vec![0,0], 0, 0), |mut acc, e| {
+	    if e >> i == acco2.0 >> i {
+		acc.0[(e >> i-1 & 1) as usize] += 1;
+		acc.2 = *e;
+		   
+	    }
+	    if e >> i == acco2.1 >> i {
+		acc.1[(e >> i-1 & 1) as usize] += 1;
+		acc.3 = *e;
+	    }
+	    acc
+	});
+	let s1 = res.0.iter().sum::<i32>();
+	let s2 = res.1.iter().sum::<i32>();
+	acco2.0 = match s1 {
+	    1 => res.2,
+	    0 | _ if res.0[0] > res.0[1] => acco2.0,
+	    _  => acco2.0 + (1 << i-1),
+	};
+	acco2.1 = match s2 {
+	    1 => res.3,
+	    0 | _ if res.1[0] <= res.1[1] => acco2.1,
+	    _  => acco2.1 + (1 << i-1),
+	};
+	acco2
+    });
+    a.0*a.1
+}
