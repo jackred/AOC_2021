a7a3034b5bd5008a87b2c7104a726a4d7d48f67b
jackred <jackred@tuta.io>
2022-01-19 15:56:24 +0000

    ADD columns for day 4 part 1
    still can't read

--- src/bin/day4.rs
+++ src/bin/day4.rs
@@ -6,37 +6,58 @@ fn main() {
     let contents = fs::read_to_string(filename)
         .expect("Something went wrong reading the file");
     let (seq, content) = contents.split_once("\n").unwrap();
-    let lines: Vec<HashSet<u32>> = content.replace("\n\n","\n")
-	.strip_prefix("\n")
-	.unwrap()
+    let content: Vec<Vec<u32>> = content.replace("\n\n","\n")
+    	.strip_prefix("\n")
+    	.unwrap()
     	.lines()
     	.map(|x| x.trim()
-	     .replace("  "," ")
-	     .split(" ")
-	     .map(|y| y.parse::<u32>().unwrap())
-	     .collect::<HashSet<u32>>())
-	.collect();
+    	     .replace("  "," ")
+    	     .split(" ")
+    	     .map(|y| y.parse::<u32>().unwrap())
+    	     .collect::<Vec<u32>>())
+    	.collect();
+    let lines = content.iter().map(|x| HashSet::from_iter(x.iter().cloned())).collect();
+    let columns = (0..content.len()).step_by(5).fold(vec![], |mut acc: Vec<HashSet<u32>>, n| {
+	content[n..n+5].into_iter().enumerate().for_each(|(m, x)| {
+	    for i in 0..5 {
+		if m == 0 {
+		    acc.push(HashSet::from([x[i]]));
+		} else {
+		    acc[n+i].insert(x[i]);
+		}
+	    }
+	});
+	acc
+    });
     let seq: Vec<u32> = seq.split(",").map(|x| x.parse::<u32>().unwrap()).collect::<Vec<u32>>();
-    println!("part 1: {}", part_1(&lines, &seq));
+    println!("part 1: {}", part_1(&lines, &columns,  &seq));
     println!("part 2: {}", part_2(&lines));  
 }
 
 
-fn part_1(input:  &Vec<HashSet<u32>>, seq: &Vec<u32>) -> u32 {
+fn part_1(lines:  &Vec<HashSet<u32>>, columns: &Vec<HashSet<u32>>, seq: &Vec<u32>) -> u32 {
     let mut hash: HashSet<u32> = HashSet::new();
     let mut found: usize = 0;
     let mut last = 0;
     for i in seq {
 	hash.insert(*i);
-	let res = input.iter().enumerate().filter(|(n, x)| hash.intersection(x).count() == 5);
+	let res = lines.iter().enumerate().filter(|(_, x)| hash.intersection(x).count() == 5);
 	let res = res.collect::<Vec<(usize, &HashSet<u32>)>>();
 	if res.len() == 1 {
 	    found = res[0].0;
 	    last = *i;
 	    break;
+	} else {
+	    let res = columns.iter().enumerate().filter(|(_, x)| hash.intersection(x).count() == 5);
+	    let res = res.collect::<Vec<(usize, &HashSet<u32>)>>();
+	    if res.len() == 1 {
+		found = res[0].0;
+		last = *i;
+		break;
+	    }
 	}
     }
-    let winner: HashSet<u32> = input[(found as f32/5.).floor() as usize * 5..(found as f32/5.).floor() as usize * 5 + 5].iter().fold(HashSet::new(), |mut acc, x| {
+    let winner: HashSet<u32> = lines[(found as f32/5.).floor() as usize * 5..(found as f32/5.).floor() as usize * 5 + 5].iter().fold(HashSet::new(), |mut acc, x| {
 	acc.extend(x);
 	acc
     });
